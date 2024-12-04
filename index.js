13dce1e1128449fe03442dbd4232468ca5ebf149
Abubakr Fozilov <abubakrfazilov98@gmail.com>
2024-12-04 07:44:16 +0200

    change icons

--- index.js
+++ index.js
@@ -2,54 +2,57 @@ function add() {
   let todoesDiv = document.querySelector(".todoes");
 
   let inputValue = document.querySelector(".todoInp").value;
+  if (inputValue === "") {
+    alert("Please enter some tast");
+  } else {
+    let todo = document.createElement("div");
+    todo = todoesDiv.appendChild(todo);
+    todo.className = "todo";
 
-  let todo = document.createElement("div");
-  todo = todoesDiv.appendChild(todo);
-  todo.className = "todo";
+    let todoText = document.createElement("h3");
+    todoText = todo.appendChild(todoText);
+    todoText.innerHTML = inputValue;
+    todoText.className = "todoText";
+    todoText.addEventListener("click", (e) => {
+      checked(e);
 
-  let todoText = document.createElement("h3");
-  todoText = todo.appendChild(todoText);
-  todoText.innerHTML = inputValue;
-  todoText.className = "todoText";
-  todoText.addEventListener("click", (e) => {
-    checked(e);
+      if (e.target.classList.length === 1) {
+        checkImg.src = "./img/unchecked.png";
+        todoText.style.textDecoration = "none";
+        todoText.style.color = "black";
+      } else if (e.target.classList.length === 2) {
+        checkImg.src = "./img/checked.png";
+        todoText.style.textDecoration = "line-through";
+        todoText.style.color = "rgb(211,211,211)";
+      }
+    });
 
-    if (e.target.classList.length === 1) {
-      checkImg.src = "./img/unchecked.png";
-      todoText.style.textDecoration = "none";
-      todoText.style.color = "black";
-    } else if (e.target.classList.length === 2) {
-      checkImg.src = "./img/checked.png";
-      todoText.style.textDecoration = "line-through";
-      todoText.style.color = "rgb(211,211,211)";
-    }
-  });
+    let checkImg = document.createElement("img");
+    checkImg = todo.appendChild(checkImg);
+    checkImg.className = "checkImg";
+    checkImg.src = "./img/unchecked.png";
+    checkImg.addEventListener("click", (e) => {
+      checked(e);
 
-  let checkImg = document.createElement("img");
-  checkImg = todo.appendChild(checkImg);
-  checkImg.className = "checkImg";
-  checkImg.src = "./img/unchecked.png";
-  checkImg.addEventListener("click", (e) => {
-    checked(e);
+      if (e.target.classList.length === 1) {
+        checkImg.src = "./img/unchecked.png";
+        todoText.style.textDecoration = "none";
+        todoText.style.color = "black";
+      } else if (e.target.classList.length === 2) {
+        checkImg.src = "./img/checked.png";
+        todoText.style.textDecoration = "line-through";
+        todoText.style.color = "rgb(211,211,211)";
+      }
+    });
 
-    if (e.target.classList.length === 1) {
-      checkImg.src = "./img/unchecked.png";
-      todoText.style.textDecoration = "none";
-      todoText.style.color = "black";
-    } else if (e.target.classList.length === 2) {
-      checkImg.src = "./img/checked.png";
-      todoText.style.textDecoration = "line-through";
-      todoText.style.color = "rgb(211,211,211)";
-    }
-  });
+    let deleteImg = document.createElement("img");
+    deleteImg.src = "./img/trash.png";
+    deleteImg = todo.appendChild(deleteImg);
+    deleteImg.className = "delete";
+    deleteImg.addEventListener("click", (e) => deleteTodo(e));
 
-  let deleteSpan = document.createElement("span");
-  deleteSpan.innerHTML = "x";
-  deleteSpan = todo.appendChild(deleteSpan);
-  deleteSpan.className = "delete";
-  deleteSpan.addEventListener("click", (e) => deleteTodo(e));
-
-  document.querySelector(".todoInp").value = "";
+    document.querySelector(".todoInp").value = "";
+  }
 }
 
 function deleteTodo(e) {
